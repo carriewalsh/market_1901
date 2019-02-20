7a7d440a93f4a7af1ff55da0b4cfb05ee5146d62
Carrie Walsh <carriewalsh@gmail.com>
2019-02-20 09:24:09 -0700

    Adds beginnings of Market and MarketTest

--- test/market_test.rb
+++ test/market_test.rb
@@ -4,5 +4,20 @@ require './lib/market'
 require './lib/vendor'
 
 class MarketTest < Minitest::Test
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
+  end
 
+  def test_market_exists
+    assert_instance_of Market, @market
+  end
+
+  def test_market_has_name
+    assert_equal "South Pearl Street Farmers Market", @market.name
+  end
+
+  def test_market_starts_with_no_vendors_by_default
+    market = Market.new("7-11")
+    assert_equal [], market.vendors
+  end
 end
