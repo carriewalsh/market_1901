6e5235ee8ac08b673ad65c6f1d8a5ae14c941841
Carrie Walsh <carriewalsh@gmail.com>
2019-02-20 09:07:55 -0700

    Clears up already given code

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -3,12 +3,12 @@ require 'minitest/pride'
 require './lib/vendor'
 
 class VendorTest < Minitest::Test
-  def test_it_exists
+  def test_vendor_exists
     vendor = Vendor.new("Rocky Mountain Fresh")
     assert_instance_of Vendor, vendor
   end
 
-  def test_it_has_a_name
+  def test_vendor_has_a_name
     vendor = Vendor.new("Rocky Mountain Fresh")
     assert_equal "Rocky Mountain Fresh", vendor.name
   end
