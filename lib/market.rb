706a803d4b9c58d93d4f0481e9731e83112b75f9
Carrie Walsh <carriewalsh@gmail.com>
2019-02-20 09:29:23 -0700

    Adds vendor methods to Market

--- lib/market.rb
+++ lib/market.rb
@@ -1,6 +1,15 @@
 class Market
-  attr_reader :name
+  attr_reader :name, :vendors
   def initialize(name)
     @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map {|vendor| vendor.name}
   end
 end
